63e0106003269a19676b233a1a3c5da8e420a658
Maxime Brunet <maximb84@gmail.com>
2024-08-26 11:12:26 +0200

    fix : [Bug report] - Bills - optimisation code

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -21,11 +21,7 @@ const row = (bill) => {
 
 // fix : [Bug report] - Bills
 const rows = (data) => {
-  if (!data) {
-    return ""
-  }
-  data.sort((a, b) => new Date(b.date) - new Date(a.date))
-  return (data.length) ? data.map(bill => row(bill)).join("") : ""
+  return (data && data.length) ? data.sort((a,b)=> new Date(b.date) - new Date(a.date)) .map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
